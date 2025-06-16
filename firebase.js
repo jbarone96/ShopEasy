a27d44869f41502adc2edaadebb3cd618015219d
Jordan Barone <j.barone024@gmail.com>
2025-06-16 16:30:43 -0400

    small tweaks

--- firebase.js
+++ firebase.js
@@ -29,5 +29,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
-
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
